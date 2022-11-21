0c794af3f9cd6654f6bf9e84c78044b8f1f22842
ai02 <zihaozhu1006@gmail.com>
2022-11-21 18:17:22 +0800

    1

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import json
 import time
 import yaml
 import os
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, send_file,render_template
 
 
 with open(os.path.join(os.path.split(__file__)[0], 'config.yaml')) as f:
@@ -14,7 +14,8 @@ act_map = {}
 
 @app.route('/')
 def index():
-    return send_file('templates/index.html')
+    return send_file('templates/index.html',act_map)
+    # return render_template('index.html',gpu_data=act_map)
 
 
 @app.route('/api/gpu', methods=['GET'])
@@ -34,4 +35,4 @@ def ping():
     return str(body)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(config['lab']['center']['port']))
+    app.run(host='0.0.0.0', port=9999)
